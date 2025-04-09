0823cdb297654ecba94d9b5914fb50feb4e309aa
sharma-adii <adityasharmaqwer123@gmail.com>
2025-04-09 11:28:05 +0530

    Create script.js

---
+++ solution4/script.js
@@ -0,0 +1,9 @@
+const people = [
+    { name: "Aditya", age: 20 },
+    { name: "Vansh", age: 19 },
+    { name: "Ansh", age: 18 },
+];
+
+people.forEach((person) =>{
+console.log(`${person.name} is ${person.age} years old`);
+})
