4f28f0703c6703195954e29a98b925cdfe88a8d7
sharma-adii <adityasharmaqwer123@gmail.com>
2025-04-09 11:30:31 +0530

    Create script.js

---
+++ solution12/script.js
@@ -0,0 +1,10 @@
+const products = [
+   { name: "laptop", price: 1500},
+   { name: "phone", price: 800},
+    { name: "tablet", price: 600},
+    { name: "monitor", price: 1400},
+    { name: "camera", price: 1200},
+
+];
+
+const  = products.filter((price) => price>=1000);
