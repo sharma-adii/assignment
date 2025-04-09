6a42a38499f63469882f28db251e1f011f466364
sharma-adii <adityasharmaqwer123@gmail.com>
2025-04-09 11:29:19 +0530

    Create script.js

---
+++ solution8/script.js
@@ -0,0 +1,8 @@
+const people = [
+    { name: "Alice", score: 80 },
+    { name: "Bob", score: 75 },
+    { name: "Charlie", score: 90 }
+];
+
+const names = people.map(person => person.name);
+console.log(names);
